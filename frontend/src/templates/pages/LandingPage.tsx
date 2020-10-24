23536f53b4539d4bfcec8e5093a317e81d5d47ff
Konrad Duleba <kon.duleba@gmail.com>
2020-10-24 19:54:24 +0200

    Added new style to Landing Page

--- frontend/src/templates/pages/LandingPage.tsx
+++ frontend/src/templates/pages/LandingPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import auth from '../../authentication/auth';
+import AppLogo from '../../images/app_logo.png';
 
 interface LandingInterface {
     history: any
@@ -9,10 +11,31 @@ const LandingPage = ({ history }: LandingInterface) => {
     const handleClick = () => auth.login(() => history.push("./app"));
 
     return (
-        <>
+        <div className="landing-page-wrapper">
+            <section className="main-view">
+                <nav>
+                    <div className="image-and-logo">
+                        <img src={AppLogo} alt="app_logo" />
+                        <p>ScheduleApp</p>
+                    </div>
+                    <ul>
+                        <Link to="#s">News</Link>
+                        <Link to="#s">About</Link>
+                        <Link to="#dd">Privacy and Security</Link>
+                        <Link to="#ss">Contact</Link>
+                    </ul>
+                    <div className="login-or-register">
+                        <p>Login</p>
+                        <p>Register</p>
+                    </div>
+                </nav>
+                <main>
+                    <p>ScheduleApp</p>
+                </main>
+            </section>
             <h1>LandingPage</h1>
             <button onClick={handleClick}>Login</button>
-        </>
+        </div>
     )
 }
 
