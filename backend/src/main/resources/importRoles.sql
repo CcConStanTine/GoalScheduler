32b66e1482b441b5f86ebb85c049f69ae4320ae3
Paweł Karpiel <pawel215999@gmail.com>
2020-11-06 15:44:41 +0100

    Updating sql import files

--- backend/src/main/resources/importRoles.sql
+++ backend/src/main/resources/importRoles.sql
@@ -1,6 +1,6 @@
-insert into role (id, name) values (NEXTVAL('role_seq'), 'ROLE_USER');
+insert into role (role_id, name) values (NEXTVAL('role_seq'), 'ROLE_USER');
 
-insert into role (id, name) values (NEXTVAL('role_seq'), 'ROLE_MODERATOR');
+insert into role (role_id, name) values (NEXTVAL('role_seq'), 'ROLE_MODERATOR');
 
-insert into role (id, name) values (NEXTVAL('role_seq'), 'ROLE_ADMIN');
+insert into role (role_id, name) values (NEXTVAL('role_seq'), 'ROLE_ADMIN');
 
