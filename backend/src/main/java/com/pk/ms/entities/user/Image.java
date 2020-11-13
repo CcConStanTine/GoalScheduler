e8a8df517875bba3b8a30b12dfd2d106b8ae5bb9
Paweł Karpiel <pawel215999@gmail.com>
2020-11-13 07:43:02 +0100

    Image Entity added - control commit before changes

---
+++ backend/src/main/java/com/pk/ms/entities/user/Image.java
@@ -0,0 +1,37 @@
+package com.pk.ms.entities.user;
+
+import javax.persistence.*;
+
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "image_seq")
+    @SequenceGenerator(name = "image_seq", sequenceName = "image_seq", allocationSize = 1)
+    private Long imageId;
+
+    private String fileUrl;
+
+    public Image() {
+    }
+
+    public Image(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public Long getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(Long imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+}
