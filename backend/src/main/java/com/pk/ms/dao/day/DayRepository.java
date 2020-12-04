833b628b7209b5226ea916c34c6b3acbd1a85773
Paweł Karpiel <pawel215999@gmail.com>
2020-12-04 21:50:15 +0100

    Refactoring Day service

--- backend/src/main/java/com/pk/ms/dao/day/DayRepository.java
+++ backend/src/main/java/com/pk/ms/dao/day/DayRepository.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DayRepository extends CrudRepository<Day, Long> {
 
-    Day findById(long id);
+    Optional<Day> findById(long id);
 
     @Query(nativeQuery = true, value = "SELECT * FROM day WHERE month_id = ?")
     List<Day> findAllByMonthId(long monthId);
 
     @Query(nativeQuery = true, value = "SELECT * FROM day WHERE day_date = ?")
-    Day findByDate(LocalDate date);
+    Optional<Day> findByDate(LocalDate date);
 
 }
