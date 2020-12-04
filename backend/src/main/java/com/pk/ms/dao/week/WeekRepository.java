3b6b044af4d7769e1c8b4aae4e01c8fbf62e7c00
Paweł Karpiel <pawel215999@gmail.com>
2020-12-04 21:19:26 +0100

    Refactoring Week service

--- backend/src/main/java/com/pk/ms/dao/week/WeekRepository.java
+++ backend/src/main/java/com/pk/ms/dao/week/WeekRepository.java
@@ -8,16 +8,17 @@ import org.springframework.stereotype.Repository;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WeekRepository extends CrudRepository<Week, Long> {
 
-    Week findById(long id);
+    Optional<Week> findById(long id);
 
     @Query(nativeQuery = true, value = "SELECT * FROM week WHERE year_id = ?")
     List<Week> findAllByYearId(long yearId);
 
     @Query(nativeQuery = true, value = "SELECT * FROM week WHERE start_date <= ?1 AND end_date >= ?1")
-    Week findByDate(LocalDate date);
+    Optional<Week> findByDate(LocalDate date);
 
 }
